73655a13877f178b0eb5e1881b628ba6d703fb4d
Danita <roboticmisfit@gmail.com>
2018-08-09 11:41:38 -0400

    added search

--- src/main/java/com/example/demo/CustomerRepository.java
+++ src/main/java/com/example/demo/CustomerRepository.java
@@ -2,5 +2,9 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    ArrayList<Customer> findByLastname(String lastname);
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,8 +3,9 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+
 
 @Controller
 public class HomeController {
@@ -14,12 +15,18 @@ public class HomeController {
     @RequestMapping("/")
     public String listCustomers(Model model){
         model.addAttribute("customers", customerRepository.findAll());
-        return "home";
+        return "list";
     }
 
     @RequestMapping("details/{customerid}")
     public String showCustomer(@PathVariable("customerid") long customerid, Model model){
         model.addAttribute("customer", customerRepository.findById(customerid).get());
         return "details";
+    }
+
+    @PostMapping("/search")
+    public String searchCustomer(String lastname, Model model){
+        model.addAttribute("customers",customerRepository.findByLastname(lastname));
+        return "list";
     }
 }
